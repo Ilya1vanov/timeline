bf74e30f682d1c3641a19f79e046f461f0d1597c
Ilya Ivanov <i.ivanov@itransition.com>
2018-12-21 21:56:43 +0300

    Add Heroku

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+
+if (process.env.PORT) {
+  app.use(forceSSL());
+}
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
